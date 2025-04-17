2610893f5cd1cd62195e6c11606e7a53beb60a44
Adarshvadakkath <adarshvadakkath2005@gmail.com>
2025-04-17 19:19:23 +0530

    first commit

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -5,6 +5,8 @@ namespace MyFirstProgram
     class Program {
         static void Main(String[] args) {
             Console.WriteLine("Hello world");
+            Console.WriteLine("i like pizza");
+            Console.Beep();
         }
      }
 }
